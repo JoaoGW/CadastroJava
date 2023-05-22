c2202b8798b8d239591c202c308b43887e58bbc2
João Pedro Ribeiro <68306736+JoaoGW@users.noreply.github.com>
2023-05-22 10:02:13 -0300

    Update ArrayListImp.java
    Correção de erro de digitação

--- ArrayListImp.java
+++ ArrayListImp.java
@@ -93,7 +93,7 @@ public class ArrayListImp implements IArmazenador
 
     }
 
-    public bool estaVaziaArrayList(){
+    public boolean estaVaziaArrayList(){
         boolean vazia = false;
 
         if(usuariosArr.isEmpty(){
